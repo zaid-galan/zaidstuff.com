30fd99c712bc28fa41dd353ba2ceb60f358765f5
zaid-galan <mrzaidkun743@gmail.com>
2025-04-16 09:10:03 -0400

    Create zaid.js

---
+++ zaid.js
@@ -0,0 +1,13 @@
+const bar = document.getElementById('bar');
+function webpageOpen(){
+    var URL = bar.value;
+    if (URL === "calculator" || URL === "Calculator"){
+        URL = "cu.html";
+    } else if (URL === "photos" || URL === "Photos" || URL === "My Photos" || URL === "my photos" || URL === "My photos"){
+        URL = "photoz.html"
+    } else if(URL === "tynker"){
+        URL = "https://tynker.com"
+    }
+    if (event.keyCode === 13){
+    window.open(URL,'Popup','top=0,left=0,width=1920,height=1080');}
+}
