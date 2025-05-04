323c02b86efc293c5c3acd3672b82378e2f42477
zaid-galan <mrzaidkun743@gmail.com>
2025-05-04 08:13:00 -0400

    dsfucyig

---
+++ zaidstuff.com files/rps.js
@@ -0,0 +1,20 @@
+function randomNumber(max) {
+    return Math.floor(Math.random() * max);
+}
+function rps(playerChoice) {
+    const choices = ['rock','paper','scissors'];
+    const computerChoice = choices[randomNumber(3)];
+    var playersChoice = choices[playerChoice];
+    var result = 'You chose ' + playersChoice + '. The computer chose ' + computerChoice + '. ';
+    if (playersChoice === computerChoice) {
+        result += "It's a tie!";
+    } else if (playersChoice === 'rock' && computerChoice === 'scissors' || 
+               playersChoice === 'paper' && computerChoice === 'rock' || 
+               playersChoice === 'scissors' && computerChoice === 'paper') {
+        result += "You win!";
+    }
+    else {
+        result += "You lose!";
+    }
+    document.getElementById('result').innerHTML = result;
+}
