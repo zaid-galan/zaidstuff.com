a3f13c3ad2d9e762467c6782aa544726c6168e29
zaid-galan <mrzaidkun743@gmail.com>
2025-05-04 14:31:13 -0400

    images

--- zaidstuff.com files/rps.js
+++ zaidstuff.com files/rps.js
@@ -2,9 +2,18 @@ function randomNumber(max) {
     return Math.floor(Math.random() * max);
 }
 function rps(playerChoice) {
+    const playerImages = [
+         'rockpsleft.png',
+         'rpapersleft.png',
+         'rpscissorsleft.png'
+    ];
+    const computerImages = [
+         'rockps.png',
+         'rpapers.png',
+         'rpscissors.png'
+    ];
     const choices = ['rock','paper','scissors'];
     const computerChoice = choices[randomNumber(3)];
-
     var playersChoice = choices[playerChoice];
     var result = 'You chose ' + playersChoice + '. The computer chose ' + computerChoice + '. ';
     if (playersChoice === computerChoice) {
@@ -18,4 +27,6 @@ function rps(playerChoice) {
         result += "You lose!";
     }
     document.getElementById('result').innerHTML = result;
+    document.getElementById('playerImage').src = playerImages[playerChoice];
+    document.getElementById('computerImage').src = computerImages[randomNumber(3)];
 }
