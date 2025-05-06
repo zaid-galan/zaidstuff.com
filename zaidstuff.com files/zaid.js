02fc574ae7feb81f363dc66efef0f8c0a87a27b9
zaid-galan <mrzaidkun743@gmail.com>
2025-05-06 13:01:42 -0400

    xvbcg

--- zaidstuff.com files/zaid.js
+++ zaidstuff.com files/zaid.js
@@ -12,8 +12,8 @@ function webpageOpen(){
     window.open(URL,'Popup','top=0,left=0,width=1920,height=1080');}
 }
 function change(){
-    document.body.setAttribute('style','background:url(MonolithAttack18.gif);background-size:cover;');
+    document.body.setAttribute('style','background:url(MonolithAttack18.gif);background-repeat:repeat;background-size:50%;');
     document.getElementById('bar').setAttribute('style','background-color:rgba(0,0,0,0.5);color:white;');
     document.getElementById('bar').setAttribute('placeholder','Enter URL or Command');
-    document.getElementsByTagName('iframe')[0].setAttribute('style','background-color:rgba(0,0,0,0.5);color:white;');
+    document.getElementsByTagName('iframe')[0].setAttribute('style','background-color:rgba(0,0,0,0.5) !important;color:white;');
 }
