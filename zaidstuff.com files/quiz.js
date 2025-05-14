d30e20f7c5e7bebc02ce0aba63cb265850829f61
zaid-galan <mrzaidkun743@gmail.com>
2025-05-14 13:21:56 -0400

    gkjl;

--- zaidstuff.com files/quiz.js
+++ zaidstuff.com files/quiz.js
@@ -28,10 +28,48 @@ var intermediateQuestions = [
     new question("What is the pH of pure water?", "7", "6", "8", "5", 0)
 ]
 var expertQuestions = [
-    new question("for loop syntax in JS?", "for (i = 0; i < 10; i++)", "for (i = 0; 10)", "for (i = 0; i < 10; i++) {}", "for (i = 0; i < 10) {}", 2),
+    new question("for loop syntax in JS?", "for (i = 0; i < 10; i++)", "for (i = 0; 10)", "for (i = 0; i < 10; i++) {}", "for (i = 0; 10) {}", 2),
     new question("What programming language is used for android development?", "Kotlin", "Python", "C++", "Swift", 0),
     new question("what is the square root of 777?", "27.87", "28.80", "29.43", "31", 1),
     new question("What is the average rainfall in africa?", "1000mm", "2000mm", "3000mm", "4000mm", 1),
-    new question("Which of the following is not a JavaScript framework?", "React", "Angular", "Django", "Vue", 2)
-    
+    new question("Which of the following is not a JavaScript framework?", "React", "Angular", "Django", "Vue", 2),
+    new question("What is the output of console.log(typeof NaN) in JavaScript?", "number", "NaN", "undefined", "object", 0),
+    new question("Which HTTP status code indicates 'Unauthorized'?", "401", "403", "404", "500", 0),
+    new question("What timezone is UTC+0?", "Greenwich Mean Time", "Eastern Standard Time", "Central Standard Time", "Pacific Standard Time", 0),
+    new question("What is the chemical symbol for silver?", "Ag", "Au", "Fe", "Pb", 0),
+    new question("How do you create a custom object in JavaScript?", "var obj = {}", "function obj() {}", "var obj = new Object()", "All of the above", 3)
 ];
+function appendParent(element, newElement) {
+    // 1. Get the parent element:
+    const parentElement = element.parentNode;
+    
+    // 2. Append the new element to the parent:
+    parentElement.appendChild(newElement);
+}
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const option3 = document.getElementById("option3");
+const option4 = document.getElementById("option4");
+option1.addEventListener("click", function() {
+    option2.unchecked = true;
+    option3.unchecked = true;
+    option4.unchecked = true;
+});
+option2.addEventListener("click", function() {
+    option1.unchecked = true;
+    option3.unchecked = true;
+    option4.unchecked = true;
+});
+option3.addEventListener("click", function() {
+    option1.unchecked = true;
+    option2.unchecked = true;
+    option4.unchecked = true;
+});
+option4.addEventListener("click", function() {
+    option1.unchecked = true;
+    option2.unchecked = true;
+    option3.unchecked = true;
+});
+function initialize(){
+
+}
