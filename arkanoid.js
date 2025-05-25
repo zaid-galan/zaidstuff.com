cc1d13802acecc1a3cfb876bfb4e2bd81126c1e5
zaid-galan <mrzaidkun743@gmail.com>
2025-05-25 10:43:08 -0400

    r4fedcg5t s7ubtny

--- arkanoid.js
+++ arkanoid.js
@@ -2,5 +2,7 @@ const startBtn = document.getElementById('startGame');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d')
 startBtn.addEventListener('click', function(){
-    document.body.style = "background-color:black;color:white;"
+    document.body.style = "background-color:black;color:white;";
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 })
