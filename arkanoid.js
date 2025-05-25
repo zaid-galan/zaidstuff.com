004fccde86bd5488d69acd6dd5de2f5a709905c4
zaid-galan <mrzaidkun743@gmail.com>
2025-05-25 09:27:43 -0400

    gfhjnbm

---
+++ arkanoid.js
@@ -0,0 +1,6 @@
+const startBtn = document.getElementById('startGame');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d')
+startBtn.addEventListener('click', function(){
+
+})
