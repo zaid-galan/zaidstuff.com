c24f1f48585024a7e38a7bfec9b3cf1dba3ba7f7
zaid-galan <devzaid77@gmail.com>
2025-06-19 17:34:42 -0400

    1234567890

---
+++ madcal.js
@@ -0,0 +1,20 @@
+Array.prototype.sum = function() {
+    return this.reduce((a, b) => a + b, 0);
+};
+
+function MAD() {
+    const numberOfNumbers = parseInt(document.getElementById("numberOfNumbers").value);
+    let values = [];
+
+    for (let i = 0; i < numberOfNumbers; i++) {
+        values.push(parseFloat(document.getElementById("option" + i).value));
+    }
+
+    const mean = values.sum() / numberOfNumbers;
+
+    let deviations = values.map(value => Math.abs(value - mean));
+
+    const mad = deviations.sum() / numberOfNumbers;
+
+    document.getElementById("result").innerHTML = "Mean Absolute Deviation: " + mad;
+}
